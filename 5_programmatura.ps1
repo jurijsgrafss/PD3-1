d79d119680107a86c22ab7d5bdb71708bfc8aa40
jurijsgrafss <jurijsgrafss@gmail.com>
2026-03-16 12:44:45 +0200

    5_programmatura.ps1

--- 5_programmatura.ps1
+++ 5_programmatura.ps1
@@ -1,7 +1,5 @@
-# Check for available software updates using winget
-
 $updates = winget upgrade | Select-String ">"
 
 $count = $updates.Count
 
-Write-Output "Sistēmā ir $count programmas, kuras nepieciešams atjaunināt."
+Write-Output "Sistema ir $count programmas, kuras nepieciesams atjauninat."
