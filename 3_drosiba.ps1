14fc500f04585b4981a5e85826994d0720cb7b2e
jurijsgrafss <jurijsgrafss@gmail.com>
2026-03-16 12:34:09 +0200

    Update 3_drosiba.ps1

--- 3_drosiba.ps1
+++ 3_drosiba.ps1
@@ -4,8 +4,8 @@ $rt = $status.RealTimeProtectionEnabled
 $scanAge = $status.QuickScanAge
 
 if (-not $rt -or $scanAge -gt 3) {
-    Write-Host "Sistēma ir apdraudēta!" -ForegroundColor Red
+    Write-Host "Sistema ir apdraudeta!" -ForegroundColor Red
 }
 else {
-    Write-Host "Sistēma ir droša." -ForegroundColor Green
+    Write-Host "Sistema ir drosa." -ForegroundColor Green
 }
