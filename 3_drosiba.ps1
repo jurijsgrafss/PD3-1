22a5f1dd235bd473bd8a2ef073d6458fa9a550a6
jurijsgrafss <jurijsgrafss@gmail.com>
2026-03-16 12:07:36 +0200

    Add files via upload

---
+++ 3_drosiba.ps1
@@ -0,0 +1,13 @@
+# Check Windows Defender protection status
+
+$status = Get-MpComputerStatus
+
+$rt = $status.RealTimeProtectionEnabled
+$scanAge = $status.QuickScanAge
+
+if (-not $rt -or $scanAge -gt 3) {
+    Write-Host "Sistēma ir apdraudēta!" -ForegroundColor Red
+}
+else {
+    Write-Host "Sistēma ir droša." -ForegroundColor Green
+}
